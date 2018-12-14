89c42b81158563f0e43d98f27f1b8e4d9873f1ea
sferdary <s.ferdary@pm.me>
2018-12-14 09:08:26 +0100

    Opdracht 4

---
+++ pages/salons/salons.php
@@ -0,0 +1,34 @@
+<?php
+/**
+ * Created by PhpStorm.
+ * User: S. Ferdary
+ * Date: 14/12/2018
+ * Time: 07:44
+ */
+?>
+<!DOCTYPE HTML>
+<html>
+<head>
+    <title>Salons - Scissor magic</title>
+    <link href="style.css" rel="stylesheet">
+
+</head>
+<body>
+<header>
+    <a href="../index.php"><img src="../../content/img/scissor-magic-logo.jpg" alt="Logo" </a>
+    <nav>
+        <ul>
+            <li><a href="salons.php">Salons</a> </li>
+            <li><a href="../producten/producten.php">Producten</a> </li>
+            <li><a href="../openingstijden/openingstijden.php">Openingstijden</a> </li>
+        </ul>
+    </nav>
+</header>
+<main>
+
+</main>
+<footer>
+
+</footer>
+</body>
+</html>
