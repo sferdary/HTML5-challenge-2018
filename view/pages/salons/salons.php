5a32b994f9632914aac2ac5003efcc73b2d696e9
sferdary <s.ferdary@pm.me>
2018-12-14 10:42:44 +0100

    Opdracht 9

--- view/pages/salons/salons.php
+++ view/pages/salons/salons.php
@@ -26,7 +26,9 @@
     </nav>
 </header>
 <main>
-
+    <h2>Onze salon</h2>
+    <img src="../../../content/img/salon/barbershop3.jpg">
+    <iframe src="https://www.google.com/maps/embed?pb=!1m18!1m12!1m3!1d2442.8939513426953!2d5.375488195672651!3d52.245309853966724!2m3!1f0!2f0!3f0!3m2!1i1024!2i768!4f13.1!3m3!1m2!1s0x0%3A0xeec3369a60ec4058!2sKapsalon+Koelewijn!5e0!3m2!1snl!2snl!4v1544780328605" width="1024" height="300" frameborder="0" style="border:0" allowfullscreen></iframe>
 </main>
 <footer>
 
