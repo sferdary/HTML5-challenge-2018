82e71c61dca121030163efe4db380aea68a13827
sferdary <s.ferdary@pm.me>
2018-12-14 07:52:20 +0100

    Opdracht 1

---
+++ index.php
@@ -0,0 +1,27 @@
+<?php
+/**
+ * Created by PhpStorm.
+ * User: S. Ferdary
+ * Date: 14/12/2018
+ * Time: 07:44
+ */
+?>
+<!DOCTYPE HTML>
+<html>
+<head>
+    <title>Scissor magic</title>
+    <link href="style.css" rel="stylesheet">
+
+</head>
+<body>
+<header>
+
+</header>
+<main>
+
+</main>
+<footer>
+
+</footer>
+</body>
+</html>
